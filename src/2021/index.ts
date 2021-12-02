82a6b81501375dbfa5812cd2c6f8d5d554fc0e51
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-12-01 18:48:22 -0600

    init 3

--- src/2021/index.ts
+++ src/2021/index.ts
@@ -1,7 +1,14 @@
 import * as days from './days';
 
+const getDayFromArgs = () => {
+  const day = process.argv[2] && process.argv[2].toLowerCase();
+  return day || 'day1';
+};
+
 const main = () => {
-  days.day1();
+  const day = getDayFromArgs();
+  console.log('day: ', day);
+  days[day]();
 };
 
 main();
