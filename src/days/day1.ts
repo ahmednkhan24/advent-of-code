4f97073c31cb0e27f7cdb2b050124b103f76c2b2
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-12-01 19:14:41 -0600

    day1 q1 done

--- src/days/day1.ts
+++ src/days/day1.ts
@@ -1,7 +1,19 @@
 import { parseTxtFile } from '../utils/parseTxtFile';
 
+const countIncreases = (data: number[]) => {
+  let numIncreases = 0;
+  for (let i = 1; i < data.length; i++) {
+    if (data[i] > data[i - 1]) {
+      numIncreases++;
+    }
+  }
+  return numIncreases;
+};
+
 export const day1 = () => {
-  console.log('day1!');
   const data = parseTxtFile('day1-input');
-  console.log('data: ', data);
+
+  // How many measurements are larger than the previous measurement?
+  const num = countIncreases(data);
+  console.log('num: ', num);
 };
