3c9ff1ecdd32fb1cd1fc41dbf17264fc42746604
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-12-01 19:10:39 -0600

    read files

--- src/days/day1.ts
+++ src/days/day1.ts
@@ -1,3 +1,7 @@
+import { parseTxtFile } from '../utils/parseTxtFile';
+
 export const day1 = () => {
   console.log('day1!');
+  const data = parseTxtFile('day1-input');
+  console.log('data: ', data);
 };
---
+++ src/utils/parseTxtFile.ts
@@ -0,0 +1,15 @@
+import fileStream from 'fs';
+
+const parseFile = (path: string) =>
+  fileStream
+    .readFileSync(path)
+    .toString()
+    .split('\n')
+    .filter((n) => n.length)
+    .map((n) => parseInt(n));
+
+export const parseTxtFile = (fileName: string = 'day1-input') => {
+  const path = `${process.cwd()}/dist/input/${fileName}.txt`;
+  const data = parseFile(path);
+  return data;
+};
