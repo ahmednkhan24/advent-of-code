4294b4041c354a45fcd052944247b383f203832b
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-12-02 22:33:19 -0600

    day2 tests

--- src/days/index.spec.ts
+++ src/days/index.spec.ts
@@ -1,4 +1,5 @@
 import * as day1 from './day1';
+import * as day2 from './day2';
 
 describe('2021 days', () => {
   describe('day1', () => {
@@ -14,6 +15,33 @@ describe('2021 days', () => {
     it('part2', () => {
       const expected = 5;
       const actual = day1.part2(input);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('day2', () => {
+    const input = [
+      'forward 5',
+      'down 5',
+      'forward 8',
+      'up 3',
+      'down 8',
+      'forward 2',
+    ];
+
+    it('part1', () => {
+      const expected = 150;
+      const { depth, position } = day2.part1(input);
+      const actual = depth * position;
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('part2', () => {
+      const expected = 900;
+      const { depth, position } = day2.part2(input);
+      const actual = depth * position;
 
       expect(actual).toEqual(expected);
     });
