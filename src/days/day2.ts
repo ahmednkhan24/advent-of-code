28cd3dbe2e223529097cee4871515bcfd471e831
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-12-02 18:49:36 -0600

    day2 q1 done

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -1,7 +1,37 @@
 import { parseTxtFile } from '../utils';
 
+const part1 = (input: string[]) => {
+  const finalHorizontalPosition = input
+    // only keep the horizontal positions
+    .filter((line) => line.toLowerCase().includes('forward'))
+    // only keep the numeric value
+    .map((line) => {
+      const amount = line.toLowerCase().split(' ')[1];
+      return parseInt(amount);
+    })
+    // sum all the values together
+    .reduce((total, current) => total + current, 0);
+
+  const finalDepth = input
+    // remove the horizontal positions
+    .filter((line) => !line.toLowerCase().includes('forward'))
+    // convert values from ['up 4', 'down 3'] to [-4, 3]
+    .map((line) => {
+      const [direction, amount] = line.toLowerCase().split(' ');
+      const amountInt = parseInt(amount);
+      return direction === 'down' ? amountInt : amountInt * -1;
+    })
+    // sum all the values together
+    .reduce((total, current) => total + current, 0);
+
+  return [finalHorizontalPosition, finalDepth];
+};
+
 export const day2 = () => {
-  console.log('hello from day2');
   const input = parseTxtFile('day2-input');
-  console.log('input: ', input);
+
+  const [position, depth] = part1(input);
+  console.log('position: ', position);
+  console.log('depth: ', depth);
+  console.log('position * depth = ', position * depth);
 };
