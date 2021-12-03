6eee02bfcb3b88aeb76bfada2f5c383192d1e8fd
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-12-02 22:23:07 -0600

    day1 tests

--- src/days/index.spec.ts
+++ src/days/index.spec.ts
@@ -1,5 +1,21 @@
+import * as day1 from './day1';
+
 describe('2021 days', () => {
-  it('tests', () => {
-    expect(1).toEqual(1);
+  describe('day1', () => {
+    const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+    it('part1', () => {
+      const expected = 7;
+      const actual = day1.part1(input);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('part2', () => {
+      const expected = 5;
+      const actual = day1.part2(input);
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
